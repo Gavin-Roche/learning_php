1bba30fcd947ebf17100ceb81ea4c43cb9a40cba
Gavin-Roche <cubergr@gmail.com>
2025-03-13 22:01:17 +0000

    Initial commit

---
+++ connection.php
@@ -0,0 +1,24 @@
+<?php
+/*
+This file is used to connect to the database
+The Parameters must be changed to accommodate the database that is set up in the user environment
+*/
+
+$host = '127.0.0.1';
+$username = 'root';
+$password = '';
+$dbname = 'k00294911_bookclub';
+
+// Tries to make a database connection
+try{
+    $db = mysqli_connect($host, $username, $password, $dbname);
+}
+// If there is an error in connecting to the database
+catch(mysqli_sql_exception $error) {
+    echo "Failed to connect to MySQL<br>";
+    echo "Error Code: " . $error->getCode() . "<br>";
+    echo "Error Message: " . $error->getMessage() . "<br>";
+    exit();
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Book Club<h1>
+        <ul>
+            <li><a href="">Add a new Member</a></li>
+            <li><a href="">Update Member Details</a></li>
+            <li><a href="">Delete a Member</a></li>
+        </ul>
+        
+        <?php
+
+        ?>
+        
+</body>
+</html>
