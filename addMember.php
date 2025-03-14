72f17bcd4a8716a48e33bcb6b169d167a2921297
Gavin-Roche <cubergr@gmail.com>
2025-03-14 17:11:46 +0000

    Add Members functionality

---
+++ addMember.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Book Club</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Book Club</h1>
+            <ul>
+                <li><a href="addMember.php">Add a new Member</a></li>
+                <li><a href="updateMember.php">Update Member Details</a></li>
+                <li><a href="deleteMember.php">Delete a Member</a></li>
+            </ul>
+            
+            <?php
+            include ('connection.php');
+            ?>
+
+            <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+                ID: <input required type="text" name="id"><br>
+                First name: <input type="text" name="firstname"><br>
+                Second name: <input type="text" name="lastname"><br>
+                Phone Number: <input type="text" name="phone"><br>
+                <input type="submit" name="submit" value="REGISTER"><br>
+
+            <?php
+                // If submitted do this
+                if(isset($_POST['submit'])){
+                    // Gets id from form
+                    $id = addslashes($_POST['id']);
+                    $sql = "SELECT * FROM member WHERE id = '$id';";
+                    $result = mysqli_query($db, $sql);
+
+                    // If there is no error
+                    if($result){
+                        // Tells the user if the record already exists
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<h3>A record with this id already exists!</h3>";
+                            exit();
+                        }
+                    }
+
+                    // Gets Information from form
+                    $firstname = addslashes($_POST["firstname"]);
+                    $lastname = addslashes($_POST["lastname"]);
+                    $phone = addslashes($_POST["phone"]);
+
+                    // Inserts the information from the form
+                    $sql = "INSERT INTO member (id, firstname, lastname, phone) VALUES ('$id', '$firstname', '$lastname', '$phone');";
+                    $result = mysqli_query($db, $sql);
+
+                    // Checks that the database updated.
+                    if($result){
+                        echo "<h3>Member added successfully!</h3>";
+                    }
+                    else{
+                        echo "<h3>An error occurred, member has not been added";
+                    }
+
+                    // Close Database
+                    mysqli_close($db);
+                }
+            ?>
+    </body>
+</html>
