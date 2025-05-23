de1a813b4ef128bfbadaa5dc1ff21780f4912ab3
Gavin-Roche <cubergr@gmail.com>
2025-05-23 11:16:06 +0100

    Added Get demonstration website

---
+++ Get_functionality.php
@@ -0,0 +1,16 @@
+<!-- Add this to the end of the url to see it in action ?first=3&second=4 -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Get functionality</title>
+    </head>
+    <body>
+        
+    <?php
+    echo "The first value is: " . $_GET["first"] . "<br>";
+    echo "The second value is: " . $_GET["second"] . "<br>";
+    ?>
+
+    </body>
+</html>
